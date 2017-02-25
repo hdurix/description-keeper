13521eaff0afd0c6ac82e2ec678e1a1f0913be14
Hippolyte Durix <hdurix@ippon.fr>
2017-02-25 15:22:27 +0100

    renaming

--- src/main/java/fr/hippo/DescriptionKeeperApplication.java
+++ src/main/java/fr/hippo/DescriptionKeeperApplication.java
@@ -8,9 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @AutoConfigurationPackage
 public class DescriptionKeeperApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DescriptionKeeperApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DescriptionKeeperApplication.class, args);
+    }
 }
-
-
