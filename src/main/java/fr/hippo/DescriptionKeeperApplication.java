74d60c75ddafba20f3db52a9e2c4a882b0212a60
Hippolyte Durix <hdurix@ippon.fr>
2017-02-25 15:19:10 +0100

    initialize project

---
+++ src/main/java/fr/hippo/DescriptionKeeperApplication.java
@@ -0,0 +1,16 @@
+package fr.hippo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@AutoConfigurationPackage
+public class DescriptionKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DescriptionKeeperApplication.class, args);
+	}
+}
+
+
